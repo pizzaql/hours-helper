bdc9ceac8408cd8be1fc3af6ceec50d1262add0e
Antoni Kepinski <xxczaki@pm.me>
2019-09-01 20:58:22 +0200

    0:00 should be 00:00

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,13 @@ pub fn get_time(number: u32, is_half_past: bool) -> String {
     let date = now.hour();
 
     if is_half_past {
-        format!("{}:30", date)
+        if date == 0 {
+            format!("0{}:30", date)
+        } else {
+            format!("{}:30", date)
+        }
+    } else if date == 0 {
+        format!("0{}:00", date)
     } else {
         format!("{}:00", date)
     }
