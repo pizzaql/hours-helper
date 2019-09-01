297f12ba2ae88caf2c3bad455059bd08a4d31218
Antoni Kepinski <xxczaki@pm.me>
2019-09-01 19:59:59 +0200

    24:00 should be 00:00

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 extern crate chrono;
 
+use chrono::{DateTime, Local, Timelike};
 use wasm_bindgen::prelude::*;
-use chrono::{DateTime, Timelike, Local};
 
 #[wasm_bindgen]
 pub fn get_time(number: u32, is_half_past: bool) -> String {
@@ -9,7 +9,13 @@ pub fn get_time(number: u32, is_half_past: bool) -> String {
     let date: u32 = now.hour() + number;
 
     if is_half_past {
-        format!("{}:30", date)
+        if date == 24 {
+            "00:30".to_string()
+        } else {
+            format!("{}:30", date)
+        }
+    } else if date == 24 {
+        "00:30".to_string()
     } else {
         format!("{}:00", date)
     }
